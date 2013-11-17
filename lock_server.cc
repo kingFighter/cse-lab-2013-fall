1b3365246572333aef688f95b1cde54c92805a1a
Kaiyang Lv(Kevin Lui) <kkmnmbj@sjtu.edu.cn>
2013-11-17 06:49:14 -0800

    [lab3-par1] modify .gitignore and to complete stub of rpc handlers

--- lock_server.cc
+++ lock_server.cc
@@ -25,8 +25,14 @@ lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
 {
   lock_protocol::status ret = lock_protocol::OK;
   printf("acquire request from clt %d\n", clt);
-  
+  return ret;
 }
 
 lock_protocol::status
 lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  lock_protocol::status ret = lock_protocol::OK;
+  printf("release request from clt %d\n", clt);
+  return ret;
+  
+}
