1978d7c0ce28e54ebb4e6d30d60b4f42140fb07e
Kaiyang Lv(Kevin Lui) <kkmnmbj@sjtu.edu.cn>
2013-11-17 06:43:39 -0800

    [lab3-part1]Register handlers for these RPCs with the RPC server object (see lock_smain.cc).

--- lock_server.cc
+++ lock_server.cc
@@ -20,4 +20,13 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   return ret;
 }
 
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  lock_protocol::status ret = lock_protocol::OK;
+  printf("acquire request from clt %d\n", clt);
+  
+}
 
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
